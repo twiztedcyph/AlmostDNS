0b2f204b7b5f8cf093cc4232ff0da6fbcabf3670
Ian Weeks <ianweeks2004@yahoo.com>
2014-07-27 17:36:42 +0100

    First commit. Project started.

---
+++ src/almostdns/AlmostDNS.java
@@ -0,0 +1,27 @@
+/*
+ * Writing this program to keep track of my external home ip address.
+ * 
+ * As it is almost impossible to get a static ip in the UK as a home
+ * customer I decided to write a program that will send an email to a
+ * given account each time my external IP address changes.
+ */
+
+
+package almostdns;
+
+/**
+ * Main
+ *
+ * @author Twiztedcyph
+ * @version 1
+ */
+public class AlmostDNS
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+}
